3887ad4c3c19197bde6e50ade53970bfc97ec4ee
Igor Konovalov <konovalov.avp@gmail.com>
2019-02-24 20:00:42 +0300

    initial

---
+++ src/index.js
@@ -0,0 +1,26 @@
+const data = {
+  type: 'retail',
+  items: [
+    {
+      name: 'Cat',
+      quantity: 1,
+      price: 40,
+    },
+    {
+      name: 'Box for Cat',
+      quantity: 2,
+      price: 100,
+    }
+  ],
+  shipping: false,
+};
+
+const calculateOrder = data => {
+  return data.items.reduce((accumulator, value) => {
+    return accumulator + value.price * value.quantity;  
+  }, 0)
+}
+
+const orderPrice = calculateOrder(data)
+
+console.log(orderPrice)
