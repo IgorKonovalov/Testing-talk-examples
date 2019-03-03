446ebb4b6b94a68e2869ec672bab2b7fb4972edf
Igor Konovalov <konovalov.avp@gmail.com>
2019-03-03 14:26:18 +0300

    replaced logic for orders calculation

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 const calculateOrder = data => {
-  return data.items
+  const itemPrice = data.items
     .filter(item => item.type !== 'shipping')
-    .reduce((accumulator, value) => {
-      return accumulator + value.price * value.quantity;
+    .reduce((accumulator, item) => {
+      return item.quantity
+        ? accumulator + item.price * item.quantity
+        : accumulator + item.price;
     }, 0);
+
+  const shippingItem = data.items.find(item => item.type === 'shipping');
+  if (itemPrice < 1000) {
+    return itemPrice + shippingItem.price;
+  }
+  return itemPrice;
 };
 
 module.exports = calculateOrder;
--- src/index.test.js
+++ src/index.test.js
@@ -1,26 +1,5 @@
 const calculateOrder = require('./');
 
-const data = {
-  items: [
-    {
-      name: 'Cat',
-      type: 'animal',
-      quantity: 1,
-      price: 40,
-    },
-    {
-      name: 'Box for Cat',
-      type: 'accessory',
-      quantity: 2,
-      price: 100,
-    },
-    {
-      type: 'shipping',
-      price: 50,
-    },
-  ],
-};
-
 describe('calculateOrder', () => {
   it('should calculate order totals correctly', () => {
     expect(calculateOrder(data)).toBe(240);
